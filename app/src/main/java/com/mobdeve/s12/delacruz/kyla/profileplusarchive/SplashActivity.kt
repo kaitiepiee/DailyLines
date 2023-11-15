a2788d963a7b6bc051165085e12a8d0cfd584aeb
Kait Tighe <kaitlyntighe@gmail.com>
2023-11-16 05:13:51 +0800

    added splash (opening) screen

---
+++ app/src/main/java/com/mobdeve/s12/delacruz/kyla/profileplusarchive/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.mobdeve.s12.delacruz.kyla.profileplusarchive
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+class SplashActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        val splashDuration = 2000 // 2 seconds
+        Handler().postDelayed({
+            // Start the main activity after the splash duration
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashDuration.toLong())
+    }
+}
