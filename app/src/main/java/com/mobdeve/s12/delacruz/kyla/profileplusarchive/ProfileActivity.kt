0fb1c93ec516219c5b11bf6062cc00d5fe0a2609
Kait Tighe <kaitlyntighe@gmail.com>
2023-11-16 08:56:28 +0800

    added logic for journal entries count

--- app/src/main/java/com/mobdeve/s12/delacruz/kyla/profileplusarchive/ProfileActivity.kt
+++ app/src/main/java/com/mobdeve/s12/delacruz/kyla/profileplusarchive/ProfileActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,12 @@ class ProfileActivity : AppCompatActivity() {
 //            startActivity(intent)
 //        }
 
+        // Show total number of entries
+        val entryListSize = intent.getIntExtra("entryListSize", 0)
+        val journalEntriesTextView = findViewById<TextView>(R.id.numJournalEntriesTv)
+        journalEntriesTextView.text = "$entryListSize"
+
+        // Exit button
         val exitButton = findViewById<ImageView>(R.id.cancelButton)
         exitButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
