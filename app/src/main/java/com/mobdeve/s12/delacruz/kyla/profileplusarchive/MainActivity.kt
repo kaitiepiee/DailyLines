6ccd5091c347c66dd1aebf9327e991c99142262e
Kait Tighe <kaitlyntighe@gmail.com>
2023-11-23 12:08:23 +0800

    Updated notifs, can retrieve token for FCM

--- app/src/main/java/com/mobdeve/s12/delacruz/kyla/profileplusarchive/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s12/delacruz/kyla/profileplusarchive/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -19,15 +20,17 @@ import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.tasks.Task
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.messaging.FirebaseMessaging
 import com.mobdeve.s12.delacruz.kyla.profileplusarchive.databinding.ActivityArchivesBinding
 import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Random
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 
 
 class MainActivity : AppCompatActivity(){
@@ -76,9 +79,25 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         AppPreferences.applyDarkModeLogic(this, R.layout.home_screen, R.layout.dark_home_screen)
 
+
         // After initializing views and Firebase Authentication
         appTitleTextView = findViewById(R.id.appTitle)
         auth = FirebaseAuth.getInstance()
+
+        // Get token for FCM
+        FirebaseMessaging.getInstance().token.addOnSuccessListener { token: String ->
+            if (!TextUtils.isEmpty(token)) {
+                Log.d(TAG, "retrieve token successful : $token")
+            } else {
+                Log.w(TAG, "token should not be null...")
+            }
+        }.addOnFailureListener { e: Exception? -> }.addOnCanceledListener {}
+            .addOnCompleteListener { task: Task<String> ->
+                Log.v(
+                    TAG,
+                    "This is the token : " + task.result
+                )
+            }
         // Check if the user is signed in
         val currentUser: FirebaseUser? = auth.currentUser
         if (currentUser != null) {
